51cd84776571eb6818eaa31711a2ada308aaf936
Hưng Đinh <hungthkk123@gmail.com>
2026-06-08 10:29:05 +0700

    Ex1 SS9

--- Ex1.sql
+++ Ex1.sql
@@ -1,21 +1,3 @@
-insert into Product(id,name,category,price,stock) values
-('Laptop Dell XPS', 'Electronics', 1500.00, 10),
-('iPhone 15 Pro', 'Electronics', 1100.00, 25),
-('Bàn phím cơ AKKO', 'Electronics', 85.50, 50),
-('Tủ lạnh Samsung', 'Appliances', 600.00, 5),
-('Lò vi sóng Sharp', 'Appliances', 120.00, 15);
+create index idx_customerid on orders(customer_id);
 
-select *from Product;
-
-select *from Product
-order by price desc
-limit 3;
-
-select *from Product
-where category='Điện tử' and price<10000000;
-
-select *from Product
-order by stock asc;
-
-
-
+explain analyse  SELECT * FROM Orders WHERE customer_id =3400000;
