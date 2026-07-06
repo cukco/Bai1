a2c2d21d1c51602e63e364ecefc469c8eb4f57f2
Hưng Đinh <hungthkk123@gmail.com>
2026-07-06 15:04:09 +0700

    Update Ex1.java

--- Ex1.java
+++ Ex1.java
@@ -1,30 +1,72 @@
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.Scanner;
-public class Ex31 {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("========= NHẬP THÔNG TIN HÓA ĐƠN =========\n");
-        System.out.print("Nhập tên khách hàng: ");
-        String tenKH = sc.nextLine();
-        System.out.print("");
-        System.out.print("Nhập tên sản phẩm: ");
-        String tenSp = sc.nextLine();
-        System.out.print("Nhập giá sản phẩm: ");
-        double Gia = sc.nextFloat();
-        System.out.print("Nhập số lượng mua: ");
-        int soluong = sc.nextInt();
-        System.out.print("Khách có thẻ thành viên? (true/false): ");
-        boolean thetv = sc.nextBoolean();
-        System.out.print("========= HÓA ĐƠN =========\n");
-        System.out.println("Khách hàng: "+tenKH);
-        System.out.println("Sản phầm: "+tenSp);
-        System.out.printf("Giá: %,.2f\n",Gia);
-        System.out.println("Số lượng: "+soluong);
-        System.out.printf("Thành tiền: %,.2f\n",Gia*soluong);
-        double discount=Gia*soluong*0.1;
-        if(thetv){
-            System.out.printf("Giảm giá: %,.2f\n",Gia*soluong*0.1);
-        }else{System.out.println("Giảm giá: 0"); discount=0;}
-        System.out.printf("Tiền VAT: %,.2f\n",Gia*soluong*0.08);
-        System.out.printf("Tiền VAT: %,.2f\n",Gia*soluong+Gia*soluong*0.08-discount);
+public class Ex61 {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        String choice;
+        ArrayList<Double> bangdiem=new ArrayList<>();
+        double max=-1, min=11,sum=0;
+        int countPass=0,countFail=0,countExcelent=0,count=0;
+        while(true){
+            System.out.println("\n********************QUẢN LÝ ĐIỂM SV********************");
+            System.out.println("1. Nhập danh sách điểm sinh viên");
+            System.out.println("2. In danh sách điểm");
+            System.out.println("3. Tính điểm trung bình của các sinh viên");
+            System.out.println("4. Tìm điểm cao nhất và thấp nhất");
+            System.out.println("5. Đếm số lượng sinh viên đạt và trượt");
+            System.out.println("6. Sắp xếp điểm tăng dần");
+            System.out.println("7. Thống kê số lượng sinh viên giỏi và xuất sắc");
+            System.out.println("8. Thoát");
+            System.out.print("Lựa chọn của bạn: ");
+            choice=sc.nextLine();
+            try{
+                int x=Integer.parseInt(choice);
+            }catch (NumberFormatException e){
+                System.out.print("Nhập lại");
+                continue;
+            }
+            if(choice.equals("8")) break;
+            else if(choice.equals("1")){
+                while (true) {
+                    String input = sc.nextLine();
+                    if (input.isEmpty()) {
+                        break;
+                    }
+                    try {
+                        double diem = Double.parseDouble(input);
+                        bangdiem.add(diem);
+                        if(diem>max) max=diem;
+                        if(diem<min) min=diem;
+                        sum+=diem;
+                        count++;
+                        if(diem>=8) countExcelent++;
+                        if(diem>=5) countPass++;
+                        if(diem<5) countFail++;
+                    } catch (NumberFormatException e) {
+                        System.out.println("Vui lòng nhập số hợp lệ hoặc nhấn enter để dừng!");
+                    }
+                }
+            }else if(choice.equals("2")){
+                for(int i=0;i<count;i++){
+                    System.out.print(bangdiem.get(i)+" ");
+                }
+            }else if(choice.equals("3")){
+                System.out.printf("Điểm trung bình: %.2f",sum/count);
+            }else if(choice.equals("4")){
+                System.out.printf("Điểm cao nhất: %.2f \nĐiểm thấp nhất: %.2f",max,min);
+            }else if(choice.equals("5")){
+                System.out.printf("Số sinh viên đạt: %d\nSố sinh viên trượt: %d",countPass,countFail);
+            }else if(choice.equals("6")){
+                bangdiem.sort(Comparator.naturalOrder());
+                System.out.println("Bảng điểm sau sắp xếp: ");
+                for(int i=0;i<count;i++){
+                    System.out.print(bangdiem.get(i)+" ");
+                }
+            }else if(choice.equals("7")){
+                System.out.printf("Số sinh viên giỏi và xuất sắc: %d",countExcelent);
+            }
+        }
+
     }
 }
